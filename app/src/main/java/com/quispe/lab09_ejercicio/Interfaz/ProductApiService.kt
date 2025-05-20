32e53098c86d1db9bc2b8d2b9820827600262cc4
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-20 15:27:09 -0500

    Creación de la interfaz ProductApiService

---
+++ app/src/main/java/com/quispe/lab09_ejercicio/Interfaz/ProductApiService.kt
@@ -0,0 +1,14 @@
+package com.quispe.lab09_ejercicio.Interfaz
+
+import com.quispe.lab09_ejercicio.Models.ProductModel
+import com.quispe.lab09_ejercicio.Models.ProductResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ProductApiService {
+    @GET("products")
+    suspend fun getProducts(): ProductResponse
+
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): ProductModel
+}
