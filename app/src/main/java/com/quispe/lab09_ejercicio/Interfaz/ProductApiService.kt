86aa8e7484bff833adcc36214a67ca96ef83328a
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-20 21:15:46 -0500

    Cambios en ProductApiService
    + en este caso se agrego los limites para añadir paginacion y que cargue por partes

--- app/src/main/java/com/quispe/lab09_ejercicio/Interfaz/ProductApiService.kt
+++ app/src/main/java/com/quispe/lab09_ejercicio/Interfaz/ProductApiService.kt
@@ -4,6 +4,7 @@ import com.quispe.lab09_ejercicio.Models.ProductModel
 import com.quispe.lab09_ejercicio.Models.ProductResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ProductApiService {
     @GET("products")
@@ -11,4 +12,12 @@ interface ProductApiService {
 
     @GET("products/{id}")
     suspend fun getProductById(@Path("id") id: Int): ProductModel
+
+    @GET("products")
+    suspend fun getProducts(
+        @Query("limit") limit: Int,
+        @Query("skip") skip: Int
+    ): ProductResponse
 }
+
+
