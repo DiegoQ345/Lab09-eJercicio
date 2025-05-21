4dc563af12ec871ea240746ea86e4036b363a946
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-20 21:04:16 -0500

    Cambios en el Main Activity
    + se agrega la función de BarraInferior
    + se agrega la función de Barrasuperior
    + se agrega la función de Contenido
    + se agrega la función de ScreenInicio

--- app/src/main/java/com/quispe/lab09_ejercicio/MainActivity.kt
+++ app/src/main/java/com/quispe/lab09_ejercicio/MainActivity.kt
@@ -4,25 +4,51 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.quispe.lab09_ejercicio.Interfaz.ProductApiService
 import com.quispe.lab09_ejercicio.ui.theme.Lab09EjercicioTheme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.quispe.lab09_ejercicio.Models.ProductModel
+import com.quispe.lab09_ejercicio.screen.ScreenProductoDetalle
+import com.quispe.lab09_ejercicio.screen.ScreenProductos
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            ProgPrincipal9()
         }
     }
 }
@@ -47,6 +73,81 @@ fun ProgPrincipal9() {
         }
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BarraSuperior() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "DummyJSON Products",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Composable
+fun BarraInferior(navController: NavHostController) {
+    NavigationBar(containerColor = Color.LightGray) {
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.Home, contentDescription = "Inicio") },
+            label = { Text("Inicio") },
+            selected = navController.currentDestination?.route == "inicio",
+            onClick = { navController.navigate("inicio") }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Outlined.List, contentDescription = "Productos") },
+            label = { Text("Productos") },
+            selected = navController.currentDestination?.route == "productos",
+            onClick = { navController.navigate("productos") }
+        )
+    }
+}
+
+@Composable
+fun Contenido(
+    pv: PaddingValues,
+    navController: NavHostController,
+    servicio: ProductApiService
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(pv)
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "inicio"
+        ) {
+            composable("inicio") { ScreenInicio() }
+            composable("productos") { ScreenProductos(navController, servicio) }
+            composable(
+                "productoDetalle/{id}",
+                arguments = listOf(navArgument("id") { type = NavType.IntType })
+            ) {
+                val id = it.arguments?.getInt("id") ?: 0
+                ScreenProductoDetalle(navController, servicio, id)
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenInicio() {
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Bienvenido a la app de Productos", style = MaterialTheme.typography.headlineMedium)
+    }
+}
+
+
 
 
 
