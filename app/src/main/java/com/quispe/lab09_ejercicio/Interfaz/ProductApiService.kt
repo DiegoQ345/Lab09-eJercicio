a5e3f87d09104592a579a3e25f866b3f05c473c3
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-05-27 22:55:13 -0500

    Se actualizo el ProductApiService
    + ahora tiene el POST PUT  yDELETE

--- app/src/main/java/com/quispe/lab09_ejercicio/Interfaz/ProductApiService.kt
+++ app/src/main/java/com/quispe/lab09_ejercicio/Interfaz/ProductApiService.kt
@@ -5,6 +5,10 @@ import com.quispe.lab09_ejercicio.Models.ProductResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ProductApiService {
     @GET("products")
@@ -18,6 +22,15 @@ interface ProductApiService {
         @Query("limit") limit: Int,
         @Query("skip") skip: Int
     ): ProductResponse
+
+    @POST("products/add")
+    suspend fun addProduct(@Body producto: ProductModel): ProductModel
+
+    @PUT("products/{id}")
+    suspend fun updateProduct(@Path("id") id: Int, @Body producto: ProductModel): ProductModel
+
+    @DELETE("products/{id}")
+    suspend fun deleteProduct(@Path("id") id: Int): Unit
 }
 
 
